fbaf79e6603b1625e6201b666860ceb497b51364
miaomingming <2415469744@qq.com>
2020-08-17 11:19:58 +0800

    登录注册完善

--- app/Http/Controllers/Admin/GoodsController.php
+++ app/Http/Controllers/Admin/GoodsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 //购物车详情控制器
 class GoodsController extends Controller
 {
-    //
+    //h
     public function index(){
         return view("/Admin/Goods/Index");
     }
--- app/Http/Controllers/Admin/RegController.php
+++ app/Http/Controllers/Admin/RegController.php
@@ -14,27 +14,27 @@ class RegController extends Controller
     //注册执行
     public function regDo(){
         $data=request()->except("_token");
+
         $validator=Validator::make($data,[
             'name'=>'required|unique:users',
-            'password'=>'required|min:6',
+            'pwd'=>'required|min:6',
         ],[
             'name.required'=>'用户名不能为空',
             'name.unique'=>'用户名重复',
             'pwd.required'=>'密码不能为空',
             'pwd.min'=>'密码至少为六位',
         ]);
+
         if($validator->fails()){
-            return redirect('/Login')->withErrors($validator)->withInput();
+            return redirect('/Reg')->withErrors($validator)->withInput();
         }
-        array_pop($data);
-        $options =[
-            'cost' =>12,
-        ];
+//        dd($data);
 //    array_pop($data);
         //$data['pwd']=password_hash($data['pwd'],PASSWORD_BCRYPT,$options);
+        //dd($data);
         $res = UserModel::insert($data);
         if($res){
-            return redirect('/login');
+            return redirect('/Login');
         }
         //dd($res);
     }
